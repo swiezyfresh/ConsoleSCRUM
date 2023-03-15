e17490b686d9f401b3a4ddee31356a10e56d7e8f
Krystian Gontarek <krystian.gontarek@gmail.com>
2023-03-16 00:23:42 +0100

    Create sprint control flow file

--- initial.py
+++ initial.py
@@ -1,4 +1,3 @@
 print("Initialize git repository")
 print("Repository test")
-
 print("ConsoleSCRUM V1 - Start Date: 16.03.2023")
---
+++ sprint_control_flow.py
@@ -0,0 +1,4 @@
+#Sprint Control Flow
+#File where operations on sprints are delegated and controlled
+#This includes sprint plans, reviews and executions
+print("Sprint Control Flow")
